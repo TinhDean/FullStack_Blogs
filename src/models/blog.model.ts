a8987dde7b96d1858f2842eb14e39605388f14fd
TinhDean <tinhminh744@gmail.com>
2026-01-24 13:20:39 +0700

    feat: init blogs module structure

--- src/models/blog.model.ts
+++ src/models/blog.model.ts
@@ -1,29 +1,29 @@
-import mongoose, { Schema, Document } from 'mongoose';
+// import mongoose, { Schema, Document } from 'mongoose';
 
-export interface BlogDocument extends Document {
-  title: string;
-  content: string;
-  isDeleted: boolean;
-}
+// export interface BlogDocument extends Document {
+//   title: string;
+//   content: string;
+//   isDeleted: boolean;
+// }
 
-const BlogSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    content: {
-      type: String,
-      required: true
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false
-    }
-  },
-  {
-    timestamps: true
-  }
-);
+// const BlogSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true
+//     },
+//     content: {
+//       type: String,
+//       required: true
+//     },
+//     isDeleted: {
+//       type: Boolean,
+//       default: false
+//     }
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
 
-export default mongoose.model<BlogDocument>('Blog', BlogSchema);
+// export default mongoose.model<BlogDocument>('Blog', BlogSchema);
